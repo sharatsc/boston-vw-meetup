b8812ff1aacd25714807192db30a4c113e9eac67
Sharat Chikkerur <schikkerur@nanigans.com>
2016-04-14 14:51:33 -0400

    LDA material

---
+++ 2016_04_14/lda.sh
@@ -0,0 +1,11 @@
+# Train LDA model
+vw --lda 2 --lda_D 8 -b 4 -d data --readable_model vw.readable.model --predictions vw.predictions --passes 10 -l 1 --lda_alpha 0.1 -k -c --initial_t 0
+# Display model file
+echo "Model file"
+cat vw.readable.model
+#Display data
+echo "Data"
+cat data
+#Display predictions
+echo "Predictions"
+cat vw.predictions | tail -8
